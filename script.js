d32e61d318afffc5c1e582378a06a573fff4b879
slawkir <slawkir1984@gmail.com>
2023-11-01 06:11:14 +0400

    feat: add main structure project (Wed, Nov 11, 2023 06:11:00)

---
+++ script.js
@@ -0,0 +1,13 @@
+const vueApp = Vue.createApp({
+  data() {
+    return {
+      result: '0',
+      numbers: [1,2,3,4,5,6,7,8,9,0]
+    };
+  },
+  // methods: {
+
+  // }
+});
+
+vueApp.mount('#app');
